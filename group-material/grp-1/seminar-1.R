c373cba2ff52ad204cc621a2fa07ae1982a7111c
Neil Lloyd <109227530+neillo88@users.noreply.github.com>
2025-01-20 17:42:28 +0000

    seminar 1, group 1 redo 2

--- group-material/grp-1/seminar-1.R
+++ group-material/grp-1/seminar-1.R
@@ -1,55 +1,60 @@
 # Seminar 1 for Group 1
 
+# define values
 x <- 3 + sin(pi/2) 
 x 
 sqrt(x)
 y <- sqrt(x)
 z <- "word"
 
-## Sequence:
+# define sequence:
 v<- seq(1,10)
 v2 <- 1:10
 
-## vectors
+# define vectors
 w <- c(12,1,2,3)
 w
 w<-sort(w)
-
 min(w)
-# second value of w
+
+# use an index to learn about the particular value of w
 w[2]
 
 # multiply all values
 w*2
-# likewise
 w/2
 
-#
+# learn about the type and length of a vector
 A <- c("1","2","5","9")
 typeof(A)
 n <- length(A)
 
+# use these elements in a loop
 for(i in 1:n){
   print(A[i])
 }
 
+# coercion: change the type of value stored
 for(i in 1:n){
   print(as.numeric(A[i]))
 }
 
+# Use packages
 
-## Packages
 #install.package("tidyverse")
 library(tidyverse)
 
 car_frame <- mpg
 
+# basic scatter plot
 ggplot(data = car_frame) +
   geom_point(mapping = aes(x=displ,y=hwy))
 
+# let the color of the dots change by class of vehicle
 ggplot(data = car_frame) +
   geom_point(mapping = aes(x=displ,y=hwy, color=class))
 
+# add multiple plots
 ggplot(data = car_frame) +
   geom_point(mapping = aes(x=displ,y=hwy)) + 
   geom_smooth(mapping = aes(x=displ,y=hwy))
